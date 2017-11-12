9f2dc1ad1c218a27a2cd52f771b3678cc86e5b37
pueppiblue <Nils.Freigang@gmail.com>
2017-11-12 17:37:33 +0100

    add form handling to GenusAdmin:newAction

--- src/AppBundle/Controller/Admin/GenusAdminController.php
+++ src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -5,6 +5,9 @@ namespace AppBundle\Controller\Admin;
 use AppBundle\Form\GenusType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @Route("/admin")
@@ -27,10 +30,18 @@ class GenusAdminController extends Controller
 
     /**
      * @Route("/genus/new", name="admin_genus_new")
+     * @param Request $request
+     * @return RedirectResponse|Response
      */
-    public function newAction()
+    public function newAction(Request $request)
     {
-        $form = $this->createForm(GenusType::class);
+        $form = $this->createForm( GenusType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->redirectToRoute('admin_genus_list');
+        }
 
         return $this->render('admin/genus/new.html.twig', [
             'genusForm' => $form->createView()
