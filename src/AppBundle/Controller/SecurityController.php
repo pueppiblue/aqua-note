c79bca1e459928b2194f50f3a33ca93f25879e82
pueppiblue <Nils.Freigang@gmail.com>
2017-11-14 14:17:44 +0100

    add login route and form

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Form\LoginForm;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class SecurityController extends Controller
+{
+
+    public function loginAction(Request $request)
+    {
+        $authUtils = $this->get('security.authentication_utils');
+
+        $lastError = $authUtils->getLastAuthenticationError();
+        $lastUsername = $authUtils->getLastUsername();
+
+        $form = $this->createForm(LoginForm::class, [
+            '_username' => $lastUsername
+        ]);
+
+        return $this->render(
+            'security/login.html.twig', [
+                'form' => $form->createView(),
+                'error' => $lastError
+            ]
+        );
+    }
+}
---
+++ src/AppBundle/Form/LoginForm.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Form;
+
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class LoginForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('_username', EmailType::class)
+            ->add('_password', PasswordType::class)
+            ;
+    }
+
+}
