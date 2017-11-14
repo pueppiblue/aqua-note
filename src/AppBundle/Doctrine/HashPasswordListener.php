00f40a795ae22b0150554f598cdd3ce737fa2900
pueppiblue <Nils.Freigang@gmail.com>
2017-11-14 17:24:07 +0100

    add doctrine eventSubscriber to encode the User password
      - use UserPasswordEncoder service
      - register it as a service tagged with doctrine.event_subscriber
      - enable bcrypt as encoder for User entity in security.yml

---
+++ src/AppBundle/Doctrine/HashPasswordListener.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace AppBundle\Doctrine;
+
+
+use AppBundle\Entity\User;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Event\LifecycleEventArgs;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoder;
+
+class HashPasswordListener implements EventSubscriber
+{
+    /**
+     * @var UserPasswordEncoder
+     */
+    private $encoder;
+
+
+    /**
+     * HashPasswordListener constructor.
+     * @param UserPasswordEncoder $encoder
+     */
+    public function __construct(UserPasswordEncoder $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    public function prePersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        if (!$entity instanceof User){
+            return;
+        }
+        if (!$entity->getPlainPassword()) {
+            return;
+        }
+
+        $this->encodePassword($entity);
+
+    }
+
+    public function preUpdate(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        if (!$entity instanceof User){
+            return;
+        }
+        if (!$entity->getPlainPassword()) {
+            return;
+        }
+
+        $this->encodePassword($entity);
+
+        // notices doctrine about the updated entity
+        $em = $args->getEntityManager();
+        $meta = $em->getClassMetadata(\get_class($entity));
+        $em->getUnitOfWork()->recomputeSingleEntityChangeSet($meta, $entity);
+    }
+
+    /**
+     * Returns an array of events this subscriber wants to listen to.
+     *
+     * @return array
+     */
+    public function getSubscribedEvents(): array
+    {
+        return ['prePersist', 'preUpdate'];
+    }
+
+    /**
+     * @param User $entity
+     */
+    private function encodePassword(User $entity): void
+    {
+        $password = $this->encoder->encodePassword($entity, $entity->getPlainPassword());
+        $entity->setPassword($password);
+    }
+}
