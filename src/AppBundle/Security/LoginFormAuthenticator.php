9984edab12a66b934555755b6afaa459fbbe5be6
pueppiblue <Nils.Freigang@gmail.com>
2017-11-14 15:52:44 +0100

    set Security::LAST_USERNAME constant on session

--- src/AppBundle/Security/LoginFormAuthenticator.php
+++ src/AppBundle/Security/LoginFormAuthenticator.php
@@ -12,6 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Router;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
@@ -62,7 +63,13 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         }
         $form = $this->formFactory->create(LoginForm::class);
         $form->handleRequest($request);
-        return $form->getData();
+        $data = $form->getData();
+
+        $request->getSession()->set(
+            Security::LAST_USERNAME, $data['_username']
+        );
+
+        return $data;
     }
 
     /**
