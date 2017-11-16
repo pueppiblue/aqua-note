4e15900dba28b1b30849989c71be541f775a28f7
pueppiblue <Nils.Freigang@gmail.com>
2017-11-16 17:30:47 +0100

    handle request and persist User in registerAction

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -2,18 +2,29 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
 use AppBundle\Form\UserRegistrationForm;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends Controller
 {
 
-    public function registerAction()
+    public function registerAction(Request $request)
     {
         $form = $this->createForm(UserRegistrationForm::class);
 
+        $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            $form->handleRequest();
+            /**@var User $user */
+            $user = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', 'Registered new User: '. $user->getEmail());
+            return $this->redirectToRoute('homepage');
         }
 
         return $this->render('user/register.html.twig', [
